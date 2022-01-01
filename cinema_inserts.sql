5e6d65d002defc1d85da3ef21492944b44a27038
230443 <230443@edu.p.lodz.pl>
2022-01-01 13:12:16 -0500

    Add inserts for Screening_type table

--- cinema_inserts.sql
+++ cinema_inserts.sql
@@ -1,6 +1,5 @@
-INSERT INTO Customers VALUES
-('random.guy@email.com'),
-('another.guy@email.com');
+INSERT INTO Customers (email) VALUES ('random.guy@email.com');
+INSERT INTO Customers (email) VALUES ('another.guy@email.com');
 
 INSERT INTO Discounts VALUES
 ('nodis', 'Full price ticket', 0),
@@ -14,6 +13,13 @@ INSERT INTO Films VALUES
 ('The Matrix Resurrections', 'Lana Wachowski', '2021-12-22'),
 ('Clifford. The big red dog', 'Walt Becker', '2021-12-03');
 
+
+INSERT INTO Films VALUES (default, 'Spider-Man: No way home', 'Jon Watts', TO_DATE('2022-01-30', 'YYYY-MM-DD'));
+INSERT INTO Films VALUES (default, 'The Lamb', 'Valdimar Jóhannsson', TO_DATE('2021-12-31', 'YYYY-MM-DD'));
+INSERT INTO Films VALUES (default, 'The Matrix Resurrections', 'Lana Wachowski', TO_DATE('2021-12-22', 'YYYY-MM-DD'));
+INSERT INTO Films VALUES (default, 'Clifford. The big red dog', 'Walt Becker', TO_DATE('2021-12-03', 'YYYY-MM-DD'));
+
+
 INSERT INTO Rooms VALUES
 (150, '2D'),
 (100, '2D'),
@@ -25,7 +31,13 @@ INSERT INTO Screenings VALUES
 (3, 1, '2021-12-21 17:30:00'),
 (4, 2, '2021-12-21 16:00:00');
 
-/*INSERT INTO Screening_types VALUES  ?*/
+INSERT INTO Screening_types VALUES ('2D', '2D', 21.00);
+INSERT INTO Screening_types VALUES ('3D', '3D', 23.00);
+INSERT INTO Screening_types VALUES ('4X2D', '4DX 2D', 35.00);
+INSERT INTO Screening_types VALUES ('4X3D', '4DX 3D', 37.00);
+INSERT INTO Screening_types VALUES ('IM2D', 'IMAX 2D', 24.00);
+INSERT INTO Screening_types VALUES ('IM3D', 'IMAX 3D', 26.00);
+
 
 INSERT INTO Sold_tickets VALUES
 (1, 1, 'nodis'),
