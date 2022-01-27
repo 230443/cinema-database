103af198542a78af7f97583600eff2c2231568cb
Daniel Baraniak <57521324+230443@users.noreply.github.com>
2022-01-28 00:01:31 +0100

    update inserts

--- cinema_inserts.sql
+++ cinema_inserts.sql
@@ -1,17 +1,10 @@
 INSERT INTO Customers (email) VALUES ('random.guy@email.com');
 INSERT INTO Customers (email) VALUES ('another.guy@email.com');
 
-INSERT INTO Discounts VALUES
-('nodis', 'Full price ticket', 0),
-('child', 'Children to 10 years old', 60),
-('stdnt', 'Students to 26 years old with valid student ID', 40),
-('senio', 'Individuals above 60 years old', 30);
-
-INSERT INTO Films VALUES
-('Spider-Man: No way home', 'Jon Watts', '2021-12-17'),
-('The Lamb', 'Valdimar Jóhannsson', '2021-12-31'),
-('The Matrix Resurrections', 'Lana Wachowski', '2021-12-22'),
-('Clifford. The big red dog', 'Walt Becker', '2021-12-03');
+INSERT INTO Discounts VALUES ('nodis', 'Full price ticket', 0.0),
+INSERT INTO Discounts VALUES ('child', 'Children to 10 years old', 0.60),
+INSERT INTO Discounts VALUES ('stdnt', 'Students to 26 years old with valid student ID', 0.40),
+INSERT INTO Discounts VALUES ('senio', 'Individuals above 60 years old', 0.30);
 
 
 INSERT INTO Films VALUES (default, 'Spider-Man: No way home', 'Jon Watts', TO_DATE('2022-01-30', 'YYYY-MM-DD'));
@@ -20,16 +13,21 @@ INSERT INTO Films VALUES (default, 'The Matrix Resurrections', 'Lana Wachowski',
 INSERT INTO Films VALUES (default, 'Clifford. The big red dog', 'Walt Becker', TO_DATE('2021-12-03', 'YYYY-MM-DD'));
 
 
-INSERT INTO Rooms VALUES
-(150, '2D'),
-(100, '2D'),
-(50, '3D');
+INSERT INTO Rooms VALUES (default, 150, '2D'),
+INSERT INTO Rooms VALUES (default, 100, '2D'),
+INSERT INTO Rooms VALUES (default, 150, '3D'),
+INSERT INTO Rooms VALUES (default, 50, '4X2D'),
+INSERT INTO Rooms VALUES (default, 70, '4X3D'),
+INSERT INTO Rooms VALUES (default, 150, 'IM2D'),
+INSERT INTO Rooms VALUES (default, 150, 'IM2D'),
+INSERT INTO Rooms VALUES (default, 130, 'IM3D'),
 
-INSERT INTO Screenings VALUES
-(1, 1, '2021-12-21 17:00:00'),
-(2, 2, '2021-12-21 17:15:00'),
-(3, 1, '2021-12-21 17:30:00'),
-(4, 2, '2021-12-21 16:00:00');
+
+INSERT INTO Screenings VALUES (default, 1, 1, TO_DATE('2021-12-21 17:00', 'YYYY-MM-DD HH24:MI')),
+INSERT INTO Screenings VALUES (default, 2, 2, TO_DATE('2021-12-21 17:15', 'YYYY-MM-DD HH24:MI')),
+INSERT INTO Screenings VALUES (default, 3, 1, TO_DATE('2021-12-21 17:30', 'YYYY-MM-DD HH24:MI')),
+INSERT INTO Screenings VALUES (default, 4, 2, TO_DATE('2021-12-21 16:00', 'YYYY-MM-DD HH24:MI')),
+
 
 INSERT INTO Screening_types VALUES ('2D', '2D', 21.00);
 INSERT INTO Screening_types VALUES ('3D', '3D', 23.00);
