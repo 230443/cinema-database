2dd7fd7f7b270c92b6ae908207ee34132ae550c9
230443 <230443@edu.p.lodz.pl>
2022-01-30 12:32:21 -0500

    Update diagram

--- tests.sql
+++ tests.sql
@@ -7,6 +7,7 @@ DECLARE
 BEGIN
     price := get_base_price(64);
     dbms_output.put_line(TO_CHAR(price));
+    price := get_base_price(1243);
 END;
 
 -- calculate price
@@ -16,6 +17,7 @@ DECLARE
 BEGIN
     price := calculate_price(64, 'stdnt');
     dbms_output.put_line(TO_CHAR(price));
+    price := calculate_price(64, 'invalid discount');
 END;
 
 -- get free places num
@@ -34,13 +36,18 @@ DECLARE
     email       customers.email%TYPE := 'random.guy@email.com';
     dis         discounts.id%TYPE := 'stdnt';
 BEGIN
-    dbms_output.put_line('sell_ticket(screening, dis, email) ');
+    dbms_output.put_line('Free places: ' || TO_CHAR(get_free_places_number(screening)));
+    dbms_output.put_line('Running sell_ticket(screening, dis, email) ');
     sell_ticket(screening, dis, email);
-    dbms_output.put_line(TO_CHAR(get_free_places_number(screening)));
-    dbms_output.put_line('sell_ticket(screening, dis) ');
+    
+    dbms_output.put_line('Free places: ' || TO_CHAR(get_free_places_number(screening)));
+    dbms_output.put_line('Running sell_ticket(screening, dis) ');
     sell_ticket(screening, dis);
-    dbms_output.put_line(TO_CHAR(get_free_places_number(screening)));
+    
+    dbms_output.put_line('Free places: ' || TO_CHAR(get_free_places_number(screening)));
+    dbms_output.put_line('Running sell_ticket(screening) ');
     sell_ticket(screening);
+    
     dbms_output.put_line('next should be an error');
     sell_ticket(screening);
 EXCEPTION
@@ -64,9 +71,8 @@ WHERE
     lower(title) LIKE '%'
                       || lower('Spider-man')
                       || '%';
-
 DECLARE
-    search_title   films.title%TYPE := 'Spider-man';
+    search_title   films.title%TYPE := 'matrix';
     title          films.title%TYPE;
     director       films.director%TYPE := 'test director modified';
     release_date   films.release_date%TYPE := '15-SEP-2020';
